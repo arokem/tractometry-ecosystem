a75cf67e2640737435843d5408c9aaa86cedf830
Ariel Rokem <arokem@uw.edu>
2025-02-07 19:23:01 +0000

    Add binder related files

---
+++ content/paths.py
@@ -0,0 +1,13 @@
+import os
+import os.path as op
+
+home = op.expanduser("~")
+os.environ["TEMPLATEFLOW_HOME"] = op.join(home, "data_", "tractometry")
+os.environ["DIPY_HOME"] = op.join(home, "data_", "tractometry")
+afq_home = op.join(home, "data_", "tractometry")
+os.makedirs(afq_home, exist_ok=True)
+os.environ["AFQ_HOME"] = afq_home
+
+import afqinsight.datasets
+afqinsight.datasets._DATA_DIR = op.join(afq_home, "afq-insight")
+
